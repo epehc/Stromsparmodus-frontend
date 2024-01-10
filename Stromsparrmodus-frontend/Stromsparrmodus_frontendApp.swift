f2221fc3399d380f741e46f4fa93f66e4a41e971
epehc <jpcardonamena@gmail.com>
2024-01-10 11:18:07 +0100

    Initial Commit

---
+++ Stromsparrmodus-frontend/Stromsparrmodus_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stromsparrmodus_frontendApp.swift
+//  Stromsparrmodus-frontend
+//
+//  Created by Jose Cardona on 10.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Stromsparrmodus_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
